153c87d78af1dc4a903f6079fa82faf29fd6a5c1
Sowmyareddy2583 <127490161+Sowmyareddy2583@users.noreply.github.com>
2024-07-15 20:18:37 +0530

    done with portfolio

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
+
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import { experiences } from "../../data/constants";
